61c7c1f78b7aa2d1c55c1eeec37caa5cebf8f549
Apenteng Joseph <apentengjoseph59@gmail.com>
2021-11-14 20:21:58 +0100

    Getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    ProductModule,
+    AppRoutingModule   
+  ],
   declarations: [
     AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule
+    WelcomeComponent    
   ],
   bootstrap: [AppComponent]
 })
